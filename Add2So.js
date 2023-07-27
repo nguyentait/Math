25d22b18e9c4bbb57dd581cdeba48bc78ce6cd40
Ta Chi Nguyen <tcnguyen93@gmail.com>
2023-07-27 17:51:38 +0700

    up

--- Add2So.js
+++ Add2So.js
@@ -13,18 +13,25 @@
 //         }
 //     }
 // }
-function add2So(a,b,mod,i){
+function add2So(a,b,mod,i,j){
         if(a.length<b.length){
-            return add2So(b,a,0,b.length)
+            return add2So(b,a,0,b.length,a.length)
         }else{
-            if(i>=0){
-                const n = parseInt( a[i]) + parseInt( b[i]) + mod;
+            if(i>0){
+                let n;
+                if(j>0){
+                    n = parseInt( a[i-1]) + parseInt( b[j-1]) + mod;
+                }
+                else{
+                    n = parseInt( a[i-1]) + mod
+                }
                 const n1 = n % 10;
                 const m1 =  n /10;
-                // n = n
+       
                 if(n>9){
                     return [ ...add2So(a,b,m1,i-1),n1]
                 }else{
+                    console.log(n)
                     return [ ...add2So(a,b,0,i-1),n]
                 }
             }else
@@ -33,4 +40,5 @@ function add2So(a,b,mod,i){
         }
     }
 
-    //add2So(["1"],["2"],0,1)
+    //add2So(["1"],["2"],0,1,1)
+    //add2So(["1","9"],["2"],0,2,1)
