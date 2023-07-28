fb9bdacc1fdc12076755c8333424f32216af9f24
Ta Chi Nguyen <tcnguyen93@gmail.com>
2023-07-28 10:23:55 +0700

    up

--- Add2So.js
+++ Add2So.js
@@ -1,21 +1,12 @@
-// function add2So(a:string [],b:string[],mod:number,i){
-//     if(a.length<b.length){
-//         return add2So(b,a,0,b.length)
-//     }else{
-//         const n:number = parseInt( a[i]) + parseInt( b[i]) + mod;
-//         const n1 = n%10;
-//         const m1 =  n /10;
-//         // n = n
-//         if(n>9){
-//             return [ ...add2So(a,b,m1,i-1),n1]
-//         }else{
-//             return [ ...add2So(a,b,0,i-1),n]
-//         }
-//     }
-// }
-function add2So(a,b,mod,i,j){
+
+function cong(n1,n2){
+    const a = n1.split("")
+    const b = n2.split("")
+    return ad(a,b,0,a.length,b.length).join("")
+}
+function ad(a,b,mod,i,j){
         if(a.length<b.length){
-            return add2So(b,a,0,b.length,a.length)
+            return ad(b,a,0,b.length,a.length)
         }else{
             if(i>0){
                 let n;
@@ -26,19 +17,55 @@ function add2So(a,b,mod,i,j){
                     n = parseInt( a[i-1]) + mod
                 }
                 const n1 = n % 10;
-                const m1 =  n /10;
-       
+                const m1 =  (n-n1) /10;
                 if(n>9){
-                    return [ ...add2So(a,b,m1,i-1),n1]
+                    return [ ...ad(a,b,m1,i-1,j-1),n1]
                 }else{
-                    console.log(n)
-                    return [ ...add2So(a,b,0,i-1),n]
+                    return [ ...ad(a,b,0,i-1,j-1),n]
                 }
             }else
                 return []
             
         }
     }
+    
+//Thuat toan cong 2 so
+function right(str,n){
+    if(str.length-n>=0){
+        return (str.substring(str.length-n,str.length))
+    }
+    else
+        return "";
+    
+}
+function left(str,n){
+    if(n<=str.length)
+        return (str.substring(0,n))
+    else return ""
+}
 
-    //add2So(["1"],["2"],0,1,1)
-    //add2So(["1","9"],["2"],0,2,1)
+function cong2(a,b,mod){
+    if(a.length<b.length){
+        return cong2(b,a,mod)
+    }
+    let r1 =  right(a,1);
+    r1 = r1===""?"0":r1;
+    let r2 = right(b,1);
+    r2 = r2===""?"0":r2;
+    let n = parseInt(r1) + parseInt(r2) + mod
+    if(n>9){
+        mod = n%10;
+        return [...cong2(
+            left(a,a.length-1),left(b,b.length-1),Math.floor(n/10)
+        ),mod.toString()]       
+    }else{
+        return [...cong(
+            left(a,a.length-1),left(b,b.length-1),0
+        ),n.toString()]
+    }
+}
+
+console.log(cong2("9","9",0).join(""))
+
+
+
---
+++ nt.js
@@ -0,0 +1,22 @@
+//* thuat toan tim so nguyen to
+var fs = require("fs")
+function SoNT(m){
+    const lstNT = [2,3]
+    for(let i = 4 ;i<m;i++){
+        let l =lstNT.filter(x=>x<=Math.sqrt(i))
+        let isNt = true;
+        l.forEach(n=>{
+            if(i%n==0){
+                isNt = false
+            }
+        })
+        if(isNt){
+            lstNT.push(i)
+        }
+    }
+    return lstNT;
+}
+fs.writeFile("data.txt",SoNT(500000).toString(),(err,data)=>{
+    if(err){console.log("loi ghi file")}
+})
+// console.log(SoNT(100))
